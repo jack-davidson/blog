020ab0d4c5b597c41809f7f1528b90edbe4aaae2
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-04-27 09:32:35 -0500

    ADDED TEMPLATING

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 #!/usr/bin/env node
 const express = require('express');
 const fs = require('fs');
-const pug = require('pug');
 
-const server = express();
+const app = express();
+
+app.use(express.static('static'));
+    
+app.set('view engine', 'ejs');
 
 const port = 8000;
 const hostname = 'localhost';
 
-server.get('/blog/:id', (req, res) => {
+app.get('/', (req, res) => {
     fs.readFile('blogs.json', 'utf8', (err, data) => {
-        res.type('json');
-        res.send(JSON.parse(data)[req.params['id']]);
+        res.render('index.ejs', {blogs: JSON.parse(data)});
     });
 });
 
-server.listen(port, hostname, () => {
+app.get('/blog/:id', (req, res) => {
+    fs.readFile('blogs.json', 'utf8', (err, data) => {
+    });
+});
+
+app.listen(port, hostname, () => {
     console.log(`blog listening at http://${hostname}:${port}`);
 });
