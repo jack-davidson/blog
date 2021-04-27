4c76459d8e3ac4167407b53235d920401c0bc37d
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-04-27 14:49:18 -0500

    Update blogs.json to include date and updated my stylesheet

--- app.js
+++ app.js
@@ -5,20 +5,32 @@ const fs = require('fs');
 const app = express();
 
 app.use(express.static('static'));
+app.set('static', __dirname + '/static');
     
 app.set('view engine', 'ejs');
 
 const port = 8000;
 const hostname = 'localhost';
 
-app.get('/', (req, res) => {
-    fs.readFile('blogs.json', 'utf8', (err, data) => {
+app.get('/', (_, res) => {
+    fs.readFile('blogs.json', 'utf8', (_, data) => {
         res.render('index.ejs', {blogs: JSON.parse(data)});
     });
 });
 
+app.get('/contact', (_, res) => {
+    res.send('/contact');
+});
+
+app.get('/about', (_, res) => {
+    res.send('/about');
+});
+
 app.get('/blog/:id', (req, res) => {
-    fs.readFile('blogs.json', 'utf8', (err, data) => {
+    fs.readFile('blogs.json', 'utf8', (_, data) => {
+        id = req.params['id'];
+        blog = JSON.parse(data)[id];
+        res.render('blog.ejs', {blog: blog});
     });
 });
 
