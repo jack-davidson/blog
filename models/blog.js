6ab306867ede8066e33c78059b90720484b8da67
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-04-30 13:25:33 -0500

    date fix update with comments

--- models/blog.js
+++ models/blog.js
@@ -7,11 +7,14 @@ function getBlogs(callback) {
     fs.readdir('blogs', (_, files) => {
         files.forEach((fileName) => {
             fileContent = fs.readFileSync('blogs/' + fileName, 'utf8');
-
-            date = new Date(fileContent.split(/\r?\n/)[0].replace('date: ', ''));
+            /* 
+             * 1) Get date from first line of fileContent and remove the date key.
+             * 2) Get body and remove date key and convert to html.
+             * 3) Get first line and remove <h1?> tag.
+             */
+            date = new Date(fileContent.split(/\r?\n/)[0].replace(/date:*/, ''));
             body = markdown.toHTML(fileContent.replace(/date\:.*\n/, ''));
             title = body.split(/\r?\n/)[0].replace(/<h1?>/, '');
-
             blogs.push({
                 title: title,
                 content: body,
