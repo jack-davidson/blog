f681cd59b46aeb87809c44f8935dab4221cf5f34
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-04-30 07:45:35 -0500

    whitespace

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 const express = require('express');
 const fs = require('fs');
-
 const routes = require('./routes/routes');
 
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const app = express();
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const port = config['port'];
+const hostname = config['hostname'];
 
 expressConfig = config['express']
-
 for (property in expressConfig) {
     app.set(property, expressConfig[property]);
 }
 
 app.use(express.static(config['static_dir']));
 
-const port = config['port'];
-const hostname = config['hostname'];
-
-
 routes(app);
-
 
 app.listen(port, hostname, () => {
     console.log(`blog listening at http://${hostname}:${port}`);
