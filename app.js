5debbab681f9713aab1185ab4474e111120eaaad
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-03 20:12:48 -0500

    uncommented static loading

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ const routes = require('./routes/routes');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const app = express();
 
-/*
 config['deployment'] == 'development' ?
     app.use(express.static(config['http']['static_dir'] || '/public')) : void(0);
-    */
 
 for (property in config['express'])
     app.set(property, config['express'][property]);
