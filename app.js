ad6a0436cd7f0b5c3088dccd7c9906c99211cac6
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-03 20:29:09 -0500

    add comments

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ const express = require('express');
 const fs = require('fs');
 const routes = require('./routes/routes');
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const app = express();
 
+/* try to load config.json */
+var config;
+try {
+    config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+} catch (err) {
+    config = JSON.parse(fs.readFileSync('config.def.json', 'utf8'));
+    /* set default deployment value to development */
+    config['deployment'] = 'development';
+}
+
+/* if deployment mode is development, set up express static file serving with default
+ * directory as /public */
 config['deployment'] == 'development' ?
     app.use(express.static(config['http']['static_dir'] || '/public')) : void(0);
 
+/* load all express settings */
 for (property in config['express'])
     app.set(property, config['express'][property]);
 
+/* get port and hostname config with default values */
 const port = config['http']['port'] || 3000;
 const hostname = config['http']['hostname'] || 'localhost';
 
