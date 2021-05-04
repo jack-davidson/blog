d8f85787762717d864e4742d3f24ead76c1082b3
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-03 20:40:27 -0500

    added models

--- models/blog.js
+++ models/blog.js
@@ -2,36 +2,34 @@ const fs = require('fs');
 const path = require('path');
 const markdown = require('markdown').markdown;
 
-function getBlogs(callback) {
-    var blogs = [];
-    fs.readdir('blogs', (_, files) => {
-        files.forEach((fileName) => {
-            fileContent = fs.readFileSync('blogs/' + fileName, 'utf8');
-            /* 
-             * 1) Get date from first line of fileContent and remove the date key.
-             * 2) Get body and remove date key and convert to html.
-             * 3) Get first line and remove <h1?> tag.
-             */
-            date = new Date(fileContent.split(/\r?\n/)[0].replace(/date:*/, ''));
-            body = markdown.toHTML(fileContent.replace(/date\:.*\n/, ''));
-            title = body.split(/\r?\n/)[0].replace(/<h1?>/, '');
-            blogs.push({
-                title: title,
-                content: body,
-                date: date
+module.exports = {
+    getBlogs: function (callback) {
+        var blogs = [];
+        fs.readdir('blogs', (_, files) => {
+            files.forEach((fileName) => {
+                fileContent = fs.readFileSync('blogs/' + fileName, 'utf8');
+                /* 
+                 * 1) Get date from first line of fileContent and remove the date key.
+                 * 2) Get body and remove date key and convert to html.
+                 * 3) Get first line and remove <h1?> tag.
+                 */
+                date = new Date(fileContent.split(/\r?\n/)[0].replace(/date:*/, ''));
+                body = markdown.toHTML(fileContent.replace(/date\:.*\n/, ''));
+                title = body.split(/\r?\n/)[0].replace(/<h1?>/, '');
+                blogs.push({
+                    title: title,
+                    content: body,
+                    date: date
+                });
             });
+            return callback(blogs.sort((a, b) => {
+                return new Date(a.date) - new Date(b.date);
+            }));
         });
-        return callback(blogs.sort((a, b) => {
-            return new Date(a.date) - new Date(b.date);
-        }));
-    });
+    },
+    getBlog: function (id, callback) {
+        this.getBlogs((blogs) => {
+            return callback(blogs[id]);
+        });
+    }
 }
-
-function getBlog(id, callback) {
-    getBlogs((blogs) => {
-        return callback(blogs[id]);
-    });
-}
-
-module.exports.getBlog = getBlog;
-module.exports.getBlogs = getBlogs;
