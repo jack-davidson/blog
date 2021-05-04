49b0cc948bb4355e22c33cb7c634e22f80f041c8
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-03 19:34:58 -0500

    update config

--- app.js
+++ app.js
@@ -6,38 +6,7 @@ const routes = require('./routes/routes');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const app = express();
 
-
-/* load server config from config, if nonexistent, use default config */
-var serverConfig;
-if ('server' in config)
-    serverConfig = config['server'];
-else {
-    serverConfig = {
-        static_dir: "public",
-        port: 3000,
-        hostname: "localhost"
-    };
-}
-
-/* load hostname number from config, if nonexistent, use default hostname */
-var port;
-if ('port' in serverConfig)
-    port = serverConfig['port'];
-else
-    port = 3000;
-
-/* load hostname from config, if nonexistent, use default hostname */
-var hostname;
-if ('hostname' in serverConfig)
-    hostname = serverConfig['hostname'];
-else
-    hostname = "localhost";
-
-/* set up static dir if not in production */
-if ('deployment' in config) {
-    if (config['deployment'] == 'development')
-        app.use(express.static(serverConfig['static_dir']));
-}
+config['deployment'] == 'development' ? app.use(express.static(config['http']['static_dir'] || '/public')): void(0);
 
 /* load express settings */
 expressConfig = config['express']
@@ -45,10 +14,13 @@ for (property in expressConfig) {
     app.set(property, expressConfig[property]);
 }
 
+const port = config['http']['port'] || 3000;
+const hostname = config['http']['hostname'] || 'localhost';
+
 /* load routes, supply it with our express object and config */
 routes(app, config);
 
 /* run server on config.port and config.hostname */
 app.listen(port, hostname, () => {
-    console.log(`blog listening at http://${hostname}:${port}`);
+    console.log(`blog listening at http://${port}:${port}`);
 });
